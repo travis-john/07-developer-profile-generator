34ad5e7c7b593e93b287c0e97ff6d8557c201990
travis-john <travis.wl.john@gmail.com>
2020-01-13 16:31:43 -0500

    inquirer functionality

--- index.js
+++ index.js
@@ -8,3 +8,42 @@ const fs = require('fs'),
       generateHTML = require('./assets/js/generate-html.js'),
       writeFileAsync = util.promisify(fs.writeFile),
       githubAsync = util.promisify(githubScraper);
+
+//inquirer function
+function askUser() {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'What is your Name?',
+      answers: 'name'
+    },
+    {
+      type: 'input',
+      name: 'What is your Github username?',
+      answers: 'username'
+    },
+    {
+      type: 'list',
+      name: 'Pick a profile color',
+      answers: 'color',
+      choices: [
+        'green',
+        'blue',
+        'pink',
+        'red',
+      ]
+    }
+  ]);
+  console.log(answers);
+}
+
+async function init() {
+  try {
+    const answers = await askUser();
+  }
+  catch (err) {
+    console.log(err);
+  }
+}
+
+init()
