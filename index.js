4a45e00ffd493a71590ccf55df6efa515320cb45
travis-john <travis.wl.john@gmail.com>
2020-01-15 09:32:00 -0500

    writing PDF functionality

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const fs = require('fs'),
       inquirer = require('inquirer'),
       htmlPDF = require('html-pdf'),
       generateHTML = require('./assets/js/generate-html.js'),
+      options = {
+        format: 'Letter',
+        orientation: 'portrait',
+        type: 'pdf'
+      }
       writeFileAsync = util.promisify(fs.writeFile),
       readFileAsync = util.promisify(fs.readFile);
 
@@ -19,7 +24,7 @@ function askUser() {
     {
       type: 'input',
       message: 'What is your Github username?',
-      answers: 'username'
+      name: 'username'
     },
     {
       type: 'list',
@@ -41,8 +46,17 @@ async function init() {
     const queryURL = `https://api.github.com/users/${answers.username}`;
     const queryURLStar = `https://api.github.com/users/${answers.username}/starred`;
     const response = await axios.get(queryURL);
+    console.log(response)
     const responseStar = await axios.get(queryURLStar);
     const html = generateHTML(answers, response, responseStar);
+    await writeFileAsync(`./assets/html/${answers.username}.html`, html);
+    console.log('Successfully wrote html file');
+    htmlPDF.create(html, options).toFile(`./assets/pdf/${answers.username}.pdf`, function(err, res){
+      if(err){
+        console.log(err)
+      }
+      console.log(`Successfully wrote PDF`);
+    })
   }
   catch (err) {
     console.log(err);
